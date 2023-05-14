097d7fc26c0b20db1e67b3e0c72638f949b88079
jayspm <62362003+jayspm@users.noreply.github.com>
2023-05-14 23:07:45 +1000

    chatbot flask python

---
+++ app.py
@@ -0,0 +1,150 @@
+import os
+import textract
+import pandas as pd
+import matplotlib.pyplot as plt
+import os.path
+from os import path
+from transformers import GPT2TokenizerFast
+from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import OpenAI
+from langchain.chains import ConversationalRetrievalChain
+from IPython.display import display
+import ipywidgets as widgets
+from flask import Flask, render_template, request
+import openai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+openai.api_key = os.environ["OPENAI_API_KEY"]
+
+os.environ["OPENAI_API_KEY"] = "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"
+loader = PyPDFLoader("./sample_data/faq.pdf")
+pages = loader.load_and_split()
+
+doc = textract.process('./faq.docx')
+with open('faq.txt', 'w', encoding='UTF-8') as f:
+    f.write(doc.decode('utf-8'))
+
+with open('faq.txt', 'r', encoding='UTF-8') as f:
+    text = f.read()
+
+tokenizer = GPT2TokenizerFast.from_pretrained("gpt2")
+
+def count_tokens(text: str) -> int:
+    return len(tokenizer.encode(text))
+
+text_splitter = RecursiveCharacterTextSplitter(
+
+    chunk_size = 512,
+    chunk_overlap  = 24,
+    length_function = count_tokens,
+)
+
+chunks = text_splitter.create_documents([text])
+
+type(chunks[0]) 
+
+token_counts = [count_tokens(chunk.page_content) for chunk in chunks]
+
+df = pd.DataFrame({'Token Count': token_counts})
+
+df.hist(bins=40, )
+
+embeddings = OpenAIEmbeddings()
+
+db = FAISS.from_documents(chunks, embeddings)
+
+qa = ConversationalRetrievalChain.from_llm(OpenAI(temperature=0.1), db.as_retriever())
+
+chat_history = []
+
+def on_submit(input):
+    query =input
+    
+    if query.lower() == 'exit':
+        print("Thank you for using the State of the Union chatbot!")
+        return
+    
+    result = qa({"question": query, "chat_history": chat_history})
+    chat_history.append((query, result['answer']))
+    
+    return (result["answer"])
+
+#print("Welcome to Swinburne Online chatbot! Type 'exit' to stop.")
+
+app = Flask(__name__)
+'''
+@app.route("/")
+def home():
+    return render_template("index1.html")
+
+@app.route("/index1.html", methods=["POST"])
+def chatbot():
+    #pass
+    
+    user_input = request.form["message"]
+    #user_input = request.form["msg"]
+    
+    prompt = f"User: {user_input}\nChatbot: "
+    chat_history = []
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        temperature=0.5,
+        max_tokens=60,
+        top_p=1,
+        frequency_penalty=0,
+        stop=["\nUser: ", "\nChatbot: "]
+    )
+
+    bot_response = on_submit(user_input)
+
+    chat_history.append(f"User: {user_input}\nChatbot: {bot_response}")
+
+    return render_template(
+        "/index1.html",
+        user_input=user_input,
+        bot_response=bot_response,
+    )
+'''
+@app.route("/")
+def home():
+    return render_template("index1.html")
+
+@app.route("/get", methods=["GET", "POST"])
+def chatbot():
+    #pass
+    
+    #user_input = request.form["message"]
+    user_input = request.form["msg"]
+    
+    prompt = f"User: {user_input}\nChatbot: "
+    chat_history = []
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        temperature=0.5,
+        max_tokens=60,
+        top_p=1,
+        frequency_penalty=0,
+        stop=["\nUser: ", "\nChatbot: "]
+    )
+
+    bot_response = on_submit(user_input)
+
+    chat_history.append(f"User: {user_input}\nChatbot: {bot_response}")
+
+    return render_template(
+        "/index1.html",
+        user_input=user_input,
+        bot_response=bot_response,
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ static/scripts/responses.js
@@ -0,0 +1,42 @@
+/*function getBotResponse(input) {
+    if (input == "How are you?") {
+        return "Good! yourself?";
+    } else if (input == "Can I ask you something?") {
+        return "Sure! what is your query?";
+    } else if (input == "Good bye") {
+        return "Have a nice day!";
+    }
+
+    // Simple responses
+    if (input == "hello") {
+        return "Hello there!";
+    } else if (input == "goodbye") {
+        return "Talk to you later!";
+    } else {
+        //return "Sorry! can you try asking something else!?";
+        return "How can I help you today?";
+    }
+}*/
+
+/*function getBotResponse(input) {
+    fetch('../../app.py', {
+        method: 'POST',
+        body: input
+      })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.text();
+      })
+      .then(data => {
+        console.log(data);
+      })
+      .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+      });
+    }*/
+
+
+
+  
